fa9ac5fc264033d7d8f8f8bf5d31bc92ce601fe6
Jack <31196@churchie.com.au>
2021-01-11 16:38:44 +0900

    dunno

--- mcwhirters-hack/source/main.cpp
+++ mcwhirters-hack/source/main.cpp
@@ -1,8 +1,43 @@
 #include <Windows.h>
+#include <memory>
+#include <iostream>
+
+BOOL HandleProcessAttach( void );
+BOOL HandleProcessDetach( void );
 
 // standard windows entry point
-int __stdcall dllmain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved ) {
+BOOL __stdcall DllMain( HINSTANCE dll, DWORD reason, LPVOID reserved ) {
 
-	return FALSE;
+	switch ( reason ) {
+
+	case DLL_PROCESS_ATTACH: return HandleProcessAttach( ); 
+	case DLL_PROCESS_DETACH: return HandleProcessDetach( );
+	default:
+		return TRUE;
+
+	}
+
+}
+
+BOOL HandleProcessAttach( void ) {
+
+	// fuck client.dll all my homies hate clientdll
+	if ( GetModuleHandleW( L"client.dll" ) != INVALID_HANDLE_VALUE )
+	{
+
+		MessageBoxW( NULL, L"Error", L"fuck off kid who gives a fuck abt legacy", MB_OK );
+		return FALSE;
+
+	}
+
+
+	return TRUE;
+
+
+}
+
+BOOL HandleProcessDetach( void ) {
+
+	return TRUE;
 
 }
